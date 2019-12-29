1ed4a6021b8aea8b4ad1cd622e101fbaca48ced0
ultram4rine <ultramarine730@gmail.com>
2019-12-29 23:33:39 +0400

    Renamed auth constants in go style

--- client_auth.go
+++ client_auth.go
@@ -1,12 +1,12 @@
 package ssh1
 
 const (
-	SSH_AUTH_RHOSTS = iota + 1
-	SSH_AUTH_RSA
-	SSH_AUTH_PASSWORD
-	SSH_AUTH_RHOSTS_RSA
-	SSH_AUTH_TIS
-	SSH_AUTH_KERBEROS
-	SSH_PASS_KERBEROS_TGT
-	SSH_PASS_AFS_TOKEN = 21
+	authRhosts = iota + 1
+	authRSA
+	authPassword
+	authRhostsRSA
+	authTIS
+	authKerberos
+	passKerberosTGT
+	passAFSToken = 21
 )
