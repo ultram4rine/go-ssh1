130064506b61c472a51a078d7c89a25874becf94
ultram4rine <ultramarine730@gmail.com>
2020-01-06 16:10:30 +0400

    Added bitmask and operations on it

---
+++ bitmask.go
@@ -0,0 +1,12 @@
+package ssh1
+
+//Bitmask represents a bitmask
+type Bitmask uint32
+
+func (m Bitmask) hasFlag(f int) bool { return m&(1<<f) != 0 }
+
+func (m *Bitmask) addFlag(f int) { *m |= 1 << f }
+
+func (m *Bitmask) removeFlag(f int) { *m &= 1 << f }
+
+func (m *Bitmask) toggleFlag(f int) { *m ^= 1 << f }
