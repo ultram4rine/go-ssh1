7dea05a3062cb869fa9fe332cc140f2bfe4e56e2
ultram4rine <ultramarine730@gmail.com>
2020-12-04 14:25:24 +0400

    draft for read/write funcs of rest ciphers

--- cipher.go
+++ cipher.go
@@ -61,6 +61,17 @@ func CreateCipherMask(ciphers ...int) *Bitmask {
 	return mask
 }
 
+// checkLength checks length of package read.
+func checkLength(length uint32) error {
+	if length <= 5 {
+		return errors.New("ssh1: invalid packet length, packet too small")
+	}
+	if length > maxPacket {
+		return errors.New("ssh1: invalid packet length, packet too large")
+	}
+	return nil
+}
+
 // See RFC, section The Binary Packet Protocol.
 const maxPacket = 262144
 
@@ -98,11 +109,8 @@ func (c *streamPacketCipher) readCipherPacket(seqNum uint32, r io.Reader) ([1]by
 	}
 
 	length := binary.BigEndian.Uint32(c.length[:])
-	if length <= 5 {
-		return packetTypeForError, nil, errors.New("ssh1: invalid packet length, packet too small")
-	}
-	if length > maxPacket {
-		return packetTypeForError, nil, errors.New("ssh1: invalid packet length, packet too large")
+	if err := checkLength(length); err != nil {
+		return packetTypeForError, nil, err
 	}
 
 	paddingLength := 8 - (length % 8)
@@ -204,6 +212,96 @@ func newCFBCipher(c cipher.Block, key, iv []byte) packetCipher {
 	return cfb
 }
 
+// readCipherPacket reads and decrypt a single packet from the reader argument.
+func (c *cfbCipher) readCipherPacket(seqNum uint32, r io.Reader) ([1]byte, []byte, error) {
+	if _, err := io.ReadFull(r, c.length[:]); err != nil {
+		return packetTypeForError, nil, err
+	}
+
+	length := binary.BigEndian.Uint32(c.length[:])
+	if err := checkLength(length); err != nil {
+		return packetTypeForError, nil, err
+	}
+
+	paddingLength := 8 - (length % 8)
+	if uint32(cap(c.padding)) < paddingLength {
+		c.padding = make([]byte, paddingLength)
+	} else {
+		c.padding = c.padding[:paddingLength]
+	}
+	if _, err := io.ReadFull(r, c.padding); err != nil {
+		return packetTypeForError, nil, err
+	}
+
+	data := make([]byte, length-5+paddingLength)
+	data = append(data, c.padding...)
+
+	if _, err := io.ReadFull(r, c.packetType[:]); err != nil {
+		return packetTypeForError, nil, err
+	}
+	data = append(data, c.packetType[:]...)
+
+	if uint32(cap(c.data)) < length-5 {
+		c.data = make([]byte, length-5)
+	} else {
+		c.data = c.data[:length-5]
+	}
+	if _, err := io.ReadFull(r, c.data); err != nil {
+		return packetTypeForError, nil, err
+	}
+	data = append(data, c.data...)
+
+	c.decrypter.XORKeyStream(data, data)
+
+	if _, err := io.ReadFull(r, c.check[:]); err != nil {
+		return packetTypeForError, nil, err
+	}
+
+	checksum := crc32.ChecksumIEEE(c.data)
+	if checksum != binary.BigEndian.Uint32(c.check[:]) {
+		return packetTypeForError, nil, errors.New("ssh1: CRC32 checksum failed")
+	}
+
+	return c.packetType, c.data, nil
+}
+
+// writeCipherPacket encrypts and writes a single packet to the writer argument.
+func (c *cfbCipher) writeCipherPacket(seqNum uint32, w io.Writer, rand io.Reader, packetType [1]byte, packet []byte) error {
+	length := len(packetType) + len(packet) + 4
+	if length > maxPacket {
+		return errors.New("ssh1: packet too large")
+	}
+
+	binary.BigEndian.PutUint32(c.length[:], uint32(length))
+
+	paddingLength := 8 - (length % 8)
+	padding := c.padding[:paddingLength]
+	if _, err := io.ReadFull(rand, padding); err != nil {
+		return err
+	}
+
+	data := padding
+	data = append(data, packetType[:]...)
+	data = append(data, packet[:]...)
+
+	checksum := crc32.ChecksumIEEE(data)
+	var checkBytes = make([]byte, 4)
+	binary.BigEndian.PutUint32(checkBytes[:], checksum)
+
+	data = append(data, checkBytes[:]...)
+
+	c.encrypter.XORKeyStream(data, data)
+
+	if _, err := w.Write(c.length[:]); err != nil {
+		return err
+	}
+	if _, err := w.Write(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func newIDEACFBCipher(key, iv []byte) (packetCipher, error) {
 	c, err := idea.NewCipher(key)
 	if err != nil {
@@ -235,6 +333,109 @@ func newCBCCipher(c cipher.Block, key, iv []byte) packetCipher {
 		decrypter: cipher.NewCBCDecrypter(c, iv),
 	}
 	return cbc
+}
+
+const (
+	cbcMinPacketSizeMultiple = 8
+	cbcMinPacketSize         = 16
+	cbcMinPaddingSize        = 4
+)
+
+func maxUInt32(a, b int) uint32 {
+	if a > b {
+		return uint32(a)
+	}
+	return uint32(b)
+}
+
+// readCipherPacket reads and decrypt a single packet from the reader argument.
+func (c *cbcCipher) readCipherPacket(seqNum uint32, r io.Reader) ([1]byte, []byte, error) {
+	if _, err := io.ReadFull(r, c.length[:]); err != nil {
+		return packetTypeForError, nil, err
+	}
+
+	length := binary.BigEndian.Uint32(c.length[:])
+	if err := checkLength(length); err != nil {
+		return packetTypeForError, nil, err
+	}
+
+	paddingLength := 8 - (length % 8)
+	if uint32(cap(c.padding)) < paddingLength {
+		c.padding = make([]byte, paddingLength)
+	} else {
+		c.padding = c.padding[:paddingLength]
+	}
+	if _, err := io.ReadFull(r, c.padding); err != nil {
+		return packetTypeForError, nil, err
+	}
+
+	data := make([]byte, length-5+paddingLength)
+	data = append(data, c.padding...)
+
+	if _, err := io.ReadFull(r, c.packetType[:]); err != nil {
+		return packetTypeForError, nil, err
+	}
+	data = append(data, c.packetType[:]...)
+
+	if uint32(cap(c.data)) < length-5 {
+		c.data = make([]byte, length-5)
+	} else {
+		c.data = c.data[:length-5]
+	}
+	if _, err := io.ReadFull(r, c.data); err != nil {
+		return packetTypeForError, nil, err
+	}
+	data = append(data, c.data...)
+
+	c.decrypter.CryptBlocks(data, data)
+
+	if _, err := io.ReadFull(r, c.check[:]); err != nil {
+		return packetTypeForError, nil, err
+	}
+
+	checksum := crc32.ChecksumIEEE(c.data)
+	if checksum != binary.BigEndian.Uint32(c.check[:]) {
+		return packetTypeForError, nil, errors.New("ssh1: CRC32 checksum failed")
+	}
+
+	return c.packetType, c.data, nil
+}
+
+// writeCipherPacket encrypts and writes a single packet to the writer argument.
+func (c *cbcCipher) writeCipherPacket(seqNum uint32, w io.Writer, rand io.Reader, packetType [1]byte, packet []byte) error {
+	length := len(packetType) + len(packet) + 4
+	if length > maxPacket {
+		return errors.New("ssh1: packet too large")
+	}
+
+	binary.BigEndian.PutUint32(c.length[:], uint32(length))
+
+	paddingLength := 8 - (length % 8)
+	padding := c.padding[:paddingLength]
+	if _, err := io.ReadFull(rand, padding); err != nil {
+		return err
+	}
+
+	data := padding
+	data = append(data, packetType[:]...)
+	data = append(data, packet[:]...)
+
+	checksum := crc32.ChecksumIEEE(data)
+	var checkBytes = make([]byte, 4)
+	binary.BigEndian.PutUint32(checkBytes[:], checksum)
+
+	data = append(data, checkBytes[:]...)
+
+	c.encrypter.CryptBlocks(data, data)
+
+	if _, err := w.Write(c.length[:]); err != nil {
+		return err
+	}
+	if _, err := w.Write(data); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func newDESCBCCipher(key, iv []byte) (packetCipher, error) {
