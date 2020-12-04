f447e459dddff85292fd3533a52e75746aae881c
ultram4rine <ultramarine730@gmail.com>
2020-12-04 15:31:38 +0400

    some bitmask tests

---
+++ bitmask_test.go
@@ -0,0 +1,40 @@
+package ssh1
+
+import (
+	"testing"
+)
+
+func TestHasFlag(t *testing.T) {
+	var m = CreateCipherMask(SSH_CIPHER_3DES)
+	v := m.hasFlag(SSH_CIPHER_3DES)
+	if !v {
+		t.Error("Expected true, got", v)
+	}
+}
+
+func TestAddFlag(t *testing.T) {
+	var m = CreateCipherMask(SSH_CIPHER_3DES)
+	m.addFlag(SSH_CIPHER_DES)
+	v := m.hasFlag(SSH_CIPHER_DES)
+	if !v {
+		t.Error("Expected true, got", v)
+	}
+}
+
+func TestRemoveFlag(t *testing.T) {
+	var m = CreateCipherMask(SSH_CIPHER_3DES)
+	m.removeFlag(SSH_CIPHER_3DES)
+	v := m.hasFlag(SSH_CIPHER_3DES)
+	if !v {
+		t.Error("Expected false, got", v)
+	}
+}
+
+func TestToggleFlag(t *testing.T) {
+	var m = CreateCipherMask(SSH_CIPHER_3DES)
+	m.toggleFlag(SSH_CIPHER_3DES)
+	v := m.hasFlag(SSH_CIPHER_3DES)
+	if v {
+		t.Error("Expected false, got", v)
+	}
+}
