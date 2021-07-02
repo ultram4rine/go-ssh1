e0446a1f85cec8c527efa4c24e9a2790b97d6fbf
ultram4rine <ultramarine730@gmail.com>
2021-07-02 14:26:32 +0400

    working crc32 for ssh1 test

--- cmd/main.go
+++ cmd/main.go
@@ -119,7 +119,17 @@ func main() {
 	data = append(data, paddingBytes...)
 	data = append(data, packetTypeBytes...)
 	data = append(data, dataBytes...)
+	fmt.Println(len(data))
 
-	checksum := crc32.ChecksumIEEE(data)
+	checksum := ssh1CRC32(data, len(data))
 	fmt.Printf("%d == %d ? %t", crc, checksum, checksum == crc)
+}
+
+// Return a 32-bit CRC of the data.
+func ssh1CRC32(data []byte, len int) uint32 {
+	var crc32val uint32
+	for i := 0; i < len; i++ {
+		crc32val = crc32.IEEETable[(crc32val^uint32(data[i]))&0xff] ^ (crc32val >> 8)
+	}
+	return crc32val
 }
