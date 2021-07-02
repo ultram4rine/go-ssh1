22c000338ec2645298b298bdb46807be3d71b476
ultram4rine <ultramarine730@gmail.com>
2021-07-02 14:25:35 +0400

    unexported crc32

--- crc32.go
+++ crc32.go
@@ -3,7 +3,7 @@ package ssh1
 import "hash/crc32"
 
 // Return a 32-bit CRC of the data.
-func CRC32(data []byte, len int) uint32 {
+func ssh1CRC32(data []byte, len int) uint32 {
 	var crc32val uint32
 	for i := 0; i < len; i++ {
 		crc32val = crc32.IEEETable[(crc32val^uint32(data[i]))&0xff] ^ (crc32val >> 8)
