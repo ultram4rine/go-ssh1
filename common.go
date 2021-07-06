7129bc036b8a88a0eb79a6612d53ccf7fd6705c2
ultram4rine <ultramarine730@gmail.com>
2021-07-06 15:49:04 +0400

    added config struct

--- common.go
+++ common.go
@@ -1,8 +1,13 @@
 package ssh1
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
 	"hash/crc32"
+	"io"
+	"net"
+	"time"
 )
 
 const protocolMajor = 1
@@ -28,4 +33,77 @@ func ssh1CRC32(data []byte, len int) uint32 {
 		crc32val = crc32.IEEETable[(crc32val^uint32(data[i]))&0xff] ^ (crc32val >> 8)
 	}
 	return crc32val
+}
+
+// HostKeyCallback is the function type used for verifying server
+// keys.  A HostKeyCallback must return nil if the host key is OK, or
+// an error to reject it. It receives the hostname as passed to Dial
+// or NewClientConn. The remote address is the RemoteAddr of the
+// net.Conn underlying the SSH connection.
+type HostKeyCallback func(hostname string, remote net.Addr, key *rsa.PublicKey) error
+
+// BannerCallback is the function type used for treat the banner sent by
+// the server. A BannerCallback receives the message sent by the remote server.
+type BannerCallback func(message string) error
+
+// Config contains configuration data common to both ServerConfig and
+// ClientConfig.
+type Config struct {
+	// Rand provides the source of entropy for cryptographic
+	// primitives. If Rand is nil, the cryptographic random reader
+	// in package crypto/rand will be used.
+	Rand io.Reader
+
+	// The allowed cipher algorithms. If unspecified then a sensible
+	// default is used.
+	Ciphers Bitmask
+
+	// User contains the username to authenticate as.
+	User string
+
+	// Auth contains possible authentication methods to use with the
+	// server. Only the first instance of a particular RFC 4252 method will
+	// be used during authentication.
+	Auth Bitmask
+
+	// HostKeyCallback is called during the cryptographic
+	// handshake to validate the server's host key. The client
+	// configuration must supply this callback for the connection
+	// to succeed. The functions InsecureIgnoreHostKey or
+	// FixedHostKey can be used for simplistic host key checks.
+	HostKeyCallback HostKeyCallback
+
+	// BannerCallback is called during the SSH dance to display a custom
+	// server's message. The client configuration can supply this callback to
+	// handle it as wished. The function BannerDisplayStderr can be used for
+	// simplistic display on Stderr.
+	BannerCallback BannerCallback
+
+	// ClientVersion contains the version identification string that will
+	// be used for the connection. If empty, a reasonable default is used.
+	Version string
+
+	// Timeout is the maximum amount of time for the TCP connection to establish.
+	//
+	// A Timeout of zero means no timeout.
+	Timeout time.Duration
+}
+
+// SetDefaults sets sensible values for unset fields in config. This is
+// exported for testing: Configs passed to SSH functions are copied and have
+// default values set automatically.
+func (c *Config) SetDefaults() {
+	if c.Rand == nil {
+		c.Rand = rand.Reader
+	}
+	if c.Ciphers == 0 {
+		c.Ciphers.addFlag(SSH_CIPHER_DES)
+		c.Ciphers.addFlag(SSH_CIPHER_3DES)
+	}
+	if c.Auth == 0 {
+		c.Auth.addFlag(SSH_AUTH_PASSWORD)
+	}
+	if c.Version == "" {
+		c.Version = packageVersion
+	}
 }
