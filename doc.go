f902c76a5ddc0fc47b301b6c88cef44161be94f4
ultram4rine <ultramarine730@gmail.com>
2021-09-24 15:00:48 +0400

    little doc

---
+++ doc.go
@@ -0,0 +1,14 @@
+/*
+Package go-ssh1 implements an SSHv1 client.
+
+SSH is a transport security protocol, an authentication protocol and a
+family of application protocols. The most typical application level
+protocol is a remote shell and this is specifically implemented.  However,
+the multiplexed nature of SSH is exposed to users that wish to support
+others.
+
+References:
+  [RFC Draft for v1.3]: https://tools.ietf.org/html/draft-ylonen-ssh-protocol-00
+  [RFC Draft for v1.5]: http://www.snailbook.com/docs/protocol-1.5.txt
+*/
+package ssh1
